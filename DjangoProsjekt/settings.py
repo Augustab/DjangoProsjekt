53ba5f26344c46354c72a461773346d7339f560b
August Asheim Birkeland <augbir@gmail.com>
2020-10-27 22:13:59 +0100

    Implementert oversikt.html, som nå viser alle utlegg hørende til en spesifikk måned.
    Koding med vemund.

--- DjangoProsjekt/settings.py
+++ DjangoProsjekt/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o51(+ta9%h7-668cb5q2h6xp653a%)c^f+4b&apx6(9rudzjcz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["easyaccounting.azurewebsites.net"]
 
 
 # Application definition
@@ -123,11 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = 'https://easyaccounting.azurewebsites.net/static'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+#STATICFILES_DIRS = [
+   # os.path.join(BASE_DIR, "static"),
+#]
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
