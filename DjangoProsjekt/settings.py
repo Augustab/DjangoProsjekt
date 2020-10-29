9ef8fbb2852d4c90a68163a1327f02bdd4117268
August Asheim Birkeland <augbir@gmail.com>
2020-10-29 21:29:07 +0100

    Tilbake fra scratch igjen

--- DjangoProsjekt/settings.py
+++ DjangoProsjekt/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
-
+#lol
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = "/home"
