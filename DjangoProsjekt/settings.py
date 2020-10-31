55709ff5eea1fa557fcc5f54f098a9c962a91712
August Asheim Birkeland <augbir@gmail.com>
2020-10-31 12:07:05 +0100

    litt databaseendringer

--- DjangoProsjekt/settings.py
+++ DjangoProsjekt/settings.py
@@ -81,13 +81,15 @@ WSGI_APPLICATION = 'DjangoProsjekt.wsgi.application'
 
 DATABASES = {
 
-    'default': dj_database_url.config(default=dj_database_url.config('DATABASE_URL'))
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-     #   'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #}
+    #'default': dj_database_url.config(default=dj_database_url.config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validat
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
